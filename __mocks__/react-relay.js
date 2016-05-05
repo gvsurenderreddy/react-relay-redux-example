777a793c84812438e1a3eb3810f9c6ddb2a5de5c
tommyjs <tomaszadrianowski@gmail.com>
2016-05-05 11:03:21 +0200

    Adding test + editing data about project  - editing data about project in package.json  - configuring jest  - adding simple test with enzyme  - adding .travis

---
+++ __mocks__/react-relay.js
@@ -0,0 +1,10 @@
+const Relay = require.requireActual('react-relay');
+
+module.exports = {
+  QL: Relay.QL,
+  Mutation: Relay.Mutation,
+  Route: Relay.Route,
+  createContainer: (component) => component
+};
+
+
---
+++ app/__tests__/App-spec.js
@@ -0,0 +1,19 @@
+jest.unmock('../App');
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import Header from '../components/Header/Header';
+import Page from '../components/Page/Page';
+
+import expect from 'expect';
+
+describe(<App />, () => {
+
+  it('renders Head and Page components inside', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header).length).toBe(1);
+    expect(wrapper.find(Page).length).toBe(1);
+  });
+
+});
